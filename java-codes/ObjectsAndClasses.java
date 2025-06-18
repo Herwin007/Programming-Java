31784b80776a555a67e29ffeb639da02e274221f
Herwin <herwin.w2005@gmail.com>
2025-06-18 14:50:30 +0530

    Add files via upload

---
+++ java-codes/ObjectsAndClasses.java
@@ -0,0 +1,8 @@
+public class ObjectsAndClasses {
+    int x=10;
+    int y=20;
+    public static void main(String[] args) {
+        ObjectsAndClasses m = new ObjectsAndClasses();
+        System.out.println(m.x+m.y);
+    }
+}
